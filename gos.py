027658542432d5a7925b14a49f9be97878776a41
Ayaan-Singh <asthana.divya@gmail.com>
2024-11-01 11:50:00 +0530

    hi

---
+++ gos.py
@@ -0,0 +1,7 @@
+number1 = float(input ("number "))
+number2 = float(input ("number2 "))
+if (number1 < number2):
+    print ("number two is larger by", number2-number1)
+else:
+    print("number one is larger by" ,number1-number2)
+
